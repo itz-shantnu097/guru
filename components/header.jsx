08e8c7d02f982df8651faed91e76c641563028b5
itz-shantnu097 <shantnufartode.21@stvincentngp.edu.in>
2025-05-19 00:26:34 +0530

    updated: logo

--- components/header.jsx
+++ components/header.jsx
@@ -27,7 +27,7 @@ export default async function Header() {
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo.png"}
+            src={"/skilllaunch.png"}
             alt="SkillLaunch Logo"
             width={200}
             height={60}
